5536f3d430ff6f8734dc6152cd857e6e0497124f
Aleksandr Guljajev <aleksandr.guljajev@abbywinters.com>
2013-07-01 16:27:00 +0200

    Linum dynamic removed from init.el

--- init.el
+++ init.el
@@ -51,8 +51,6 @@
 (put 'erase-buffer 'disabled nil)
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks"))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(custom-safe-themes (quote ("d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" default)))
+)
