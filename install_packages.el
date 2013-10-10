f2958bda293ed225415eda697d61020dcee07258
Aleksandr Guljajev <aleksandr.guljajev@abbywinters.com>
2013-10-10 11:14:19 +0200

    Removed gtags package from install-packages list, replaced by helm-gtags

--- install_packages.el
+++ install_packages.el
@@ -2,7 +2,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(paredit rainbow-delimiters clojure-mode nrepl framemove ac-nrepl dired-details  dired-details+ helm auto-complete golden-ratio gtags haml-mode magit nrepl json smex bookmark+ ido-ubiquitous php-mode helm-ls-git monokai-theme helm-gtags)
+(defvar my-packages '(paredit rainbow-delimiters clojure-mode nrepl framemove ac-nrepl dired-details  dired-details+ helm auto-complete golden-ratio haml-mode magit nrepl json smex bookmark+ ido-ubiquitous php-mode helm-ls-git monokai-theme helm-gtags)
 
   "A list of packages to ensure are installed at launch.")
 
