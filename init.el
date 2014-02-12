7a451d8e1ec9686dc5b9ecc75b3baf062619e46d
Aleksandr Guljajev <aleksandr.guljajev@gmbill.com>
2014-02-12 14:14:35 +0100

    Disabled item highlight

--- init.el
+++ init.el
@@ -31,7 +31,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(magit-item-highlight ((t nil))))
 
 
 (setq mac-command-modifier 'control)
@@ -49,7 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(bmkp-last-as-first-bookmark-file "/home/AW/aleksandr.guljajev/.emacs.d/bookmarks")
  '(custom-safe-themes (quote ("d24e10524bb50385f7631400950ba488fa45560afcadd21e6e03c2f5d0fad194" default)))
  '(dired-dwim-target t)
  '(linum-format (quote dynamic)))
