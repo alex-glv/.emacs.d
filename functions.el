7a1cff8e6e2df48767a5c62952224d371fa3d65f
Aleksandr Guljajev <aleksandr.guljajev@gmbill.com>
2014-11-12 15:09:06 +0100

    Toggles dired-dwim-target value

--- functions.el
+++ functions.el
@@ -1,2 +1,10 @@
 (defun disable-magit-highlight-in-buffer () 
   (face-remap-add-relative 'magit-item-highlight '()))
+
+(defun toggle-dired-dwim ()
+  (interactive)
+  (let ((crnt-value dired-dwim-target))
+    (message "Flipping dired-dwim-target %s value" crnt-value)
+    (if (eq crnt-value nil)
+	(setq dired-dwim-target t)
+      (setq dired-dwim-target nil))))
