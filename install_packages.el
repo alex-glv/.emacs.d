3df4a9c23e50d750a104eff09817799d6b1dcaba
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-01-30 22:01:10 +0100

    Enabling js2 mode

--- install_packages.el
+++ install_packages.el
@@ -2,7 +2,33 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(paredit rainbow-delimiters clojure-mode cider company company-go framemove dired+ helm golden-ratio haml-mode magit json smex bookmark+ ido-ubiquitous php-mode virtualenv helm-ls-git monokai-theme go-mode go-play exec-path-from-shell multiple-cursors apache-mode dockerfile-mode)
+(defvar my-packages '(paredit
+		      rainbow-delimiters
+		      clojure-mode
+		      cider
+		      company
+		      company-go
+		      framemove
+		      dired+
+		      helm
+		      haml-mode
+		      magit
+		      json
+		      smex
+		      bookmark+
+		      ido-ubiquitous
+		      php-mode
+		      virtualenv
+		      helm-ls-git
+		      zenburn-theme
+		      go-mode
+		      go-play
+		      exec-path-from-shell
+		      multiple-cursors
+		      apache-mode
+		      dockerfile-mode
+		      js2-mode
+		      web-mode)
 
   "A list of packages to ensure are installed at launch.")
 
