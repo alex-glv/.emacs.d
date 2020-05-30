5745d9de6946680b237e4fdcbb6da18fc493f67c
Aleksandr Guljajev <s@itchyknowsdevs.me>
2020-05-30 10:16:07 +0200

    Removes junk

--- custom.el
+++ custom.el
@@ -1,11 +1,3 @@
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:foreground "#DCDCCC" :background "#3F3F3F" :family "Iosevka Term" :foundry "nil" :slant normal :weight normal :height 180 :width normal))))
- '(mode-line ((t (:background "#000000" :foreground "#F5F5F5" :bfox (:line-width 1 :color "#212121" :style unspecified)))))
- '(scroll-bar ((t (:underline nil :weight normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,51 +5,19 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(cider-cljs-lein-repl
-   "(do (require 'weasel.repl.websocket) (cemerick.piggieback/cljs-repl (weasel.repl.websocket/repl-env :ip \"127.0.0.1\" :port 9001)))")
- '(cider-refresh-before-fn nil)
- '(company-go-begin-after-member-access t)
- '(company-go-show-annotation t)
- '(company-idle-delay 0.2)
- '(cursor-type (quote box))
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
-    ("54f2d1fcc9bcadedd50398697618f7c34aceb9966a6cbaa99829eb64c0c1f3ca" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
+    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" default)))
  '(fci-rule-color "#383838")
- '(grep-files-aliases
-   (quote
-    (("php" . "*.inc *.php")
-     ("all" . "* .*")
-     ("el" . "*.el")
-     ("ch" . "*.[ch]")
-     ("c" . "*.c")
-     ("cc" . "*.cc *.cxx *.cpp *.C *.CC *.c++")
-     ("cchh" . "*.cc *.[ch]xx *.[ch]pp *.[CHh] *.CC *.HH *.[ch]++")
-     ("hh" . "*.hxx *.hpp *.[Hh] *.HH *.h++")
-     ("h" . "*.h")
-     ("l" . "[Cc]hange[Ll]og*")
-     ("m" . "[Mm]akefile*")
-     ("tex" . "*.tex")
-     ("texi" . "*.texi")
-     ("asm" . "*.[sS]"))))
- '(ivy-format-function (quote ivy-format-function-arrow))
- '(linum-format (quote dynamic))
- '(menu-bar-mode nil)
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(org-clock-rounding-minutes 5)
- '(org-support-shift-select nil)
- '(org-time-stamp-rounding-minutes (quote (5 15)))
  '(package-selected-packages
    (quote
-    (evil-mc evil-magit evil-mode groovy-mode godoctor go-guru smex terraform-mode racer flycheck-rust rust-mode counsel ivy clj-refactor clj-refacor inf-clojure avy company-flx which-key undo-tree flycheck golden-ratio yaml-mode markdown-mode flx-ido ido-ubiquitous cider clojure-mode exec-path-from-shell paredit expand-region projectile magit rainbow-mode rainbow-delimiters zenburn-theme with-editor use-package magit-popup company)))
+    (racer company-restclient restclient flycheck-rust rust-mode which-key flycheck golden-ratio yaml-mode markdown-mode smex counsel ivy terraform-mode exec-path-from-shell expand-region clj-refactor projectile evil-magit magit godoctor go-guru rainbow-mode rainbow-delimiters use-package)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
- '(scroll-bar-mode nil)
- '(search-nonincremental-instead nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map
    (quote
@@ -80,3 +40,9 @@
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
